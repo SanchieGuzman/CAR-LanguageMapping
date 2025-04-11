d4f6cfa218ab91553f3a2ff4d50f7bbf37c9791a
Stephen Coloma <2232847@slu.edu.ph>
2025-04-12 01:14:06 +0800

    implemented search functionailty

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,12 +1,94 @@
+'use client'
+
 import { Input } from "./ui/input"
+import { getIdByMunicipalityName, municipalityIdMapping } from "../lib/getIdByMunicipalityName";
+import { Search } from 'lucide-react'
+import { useState, useRef, useEffect } from "react";
+import debounce from "lodash.debounce";
+import { useMunicipalityStore } from "../store/municipality-store";
+const municipalities = Array.from(municipalityIdMapping.keys()); // municipality province names
 
 function SearchBar() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const wrapperRef = useRef(null);
+  const fetchMunicipalityDataById = useMunicipalityStore((state)=> state.fetchMunicipalityDataById);
+
+  // search bar onchange
+  const debouncedOnChange = debounce((e) => {
+    const searchKey = e.target.value.trim().replaceAll('-', '');
+    
+    if(searchKey.length > 0) {
+      setIsOpen(true)
+      //filter based on input
+      const results = municipalities.filter((name) => {return name.toLowerCase().includes(searchKey)})
+      setSearchResults(results);      
+    }else{
+      setIsOpen(false)
+    };
+
+  }, 250);
+
+  // clicking a place in search results
+  const handleOnClick = (chosenMunicipality) => {
+    const id = getIdByMunicipalityName(chosenMunicipality);
+    setIsOpen(false);
+    fetchMunicipalityDataById(id);
+  }
+
+  // closes the search results div when it loses focus
+  useEffect(() => {
+    const handleClickOutside = (event) =>  {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+  
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <Input 
-      type="text" 
-      placeholder="Search a municipality here" 
-      className="max-w-md"
-    />
+    // main container
+    <div className="max-w-xl w-full md:w-[50%] relative">
+      {/* search bar */}
+      <div className="relative">
+        <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search here..."
+          className="pl-10 pr-4 py-2"
+          onChange={debouncedOnChange}
+          onFocus={() => searchResults.length > 0 && setIsOpen(true)}
+        />
+      </div>
+
+      {/* search results container*/}
+      {isOpen && 
+        <div ref={wrapperRef} className="absolute z-10 h-fit max-h-82 w-full bg-primary-foreground shadow-lg rounded-b-lg border-2 mt-1 overflow-y-auto">
+          <ul>
+            {searchResults.length > 0 ? 
+              searchResults
+                .map((result, index) => (
+                  <li 
+                    key={index}
+                    className="min-h-8 max-h-8 px-2 py-2 hover:bg-muted cursor-pointer flex items-center gap-2 capitalize"
+                    onClick={()=>handleOnClick(result)}
+                  >
+                    <Search className="h-4"></Search>
+                    {result.replace(/-(?=[^-]*$)/, ', ')}
+                  </li>
+                ))
+                : 
+                <li className="p-2 text-muted-foreground">No results found</li>
+            }
+            
+          </ul>
+        </div>
+      }
+    </div>
   );
 }
 
